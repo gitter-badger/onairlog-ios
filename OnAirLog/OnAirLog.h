31be3e823e6e0eaa6aee78eef7ff27e10d8da06b
Atsushi Nagase <a@ngs.io>
2014-10-13 02:48:03 +0900

    [WIP] Framework common classes

---
+++ OnAirLog/OnAirLog.h
@@ -0,0 +1,33 @@
+//
+//  OnAirLog.h
+//  OnAirLog
+//
+//  Created by Atsushi Nagase on 10/13/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnAirLog.
+FOUNDATION_EXPORT double OnAirLogVersionNumber;
+
+//! Project version string for OnAirLog.
+FOUNDATION_EXPORT const unsigned char OnAirLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnAirLog/PublicHeader.h>
+
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/UIKit+AFNetworking.h>
+#import <BlocksKit/BlocksKit.h>
+#import <CoreData/CoreData.h>
+#import <UIKit/UIKit.h>
+#import <GoogleAnalytics-iOS-SDK/GAI.h>
+#import <GoogleAnalytics-iOS-SDK/GAIFields.h>
+#import <GoogleAnalytics-iOS-SDK/GAIDictionaryBuilder.h>
+#import <StoreKit/StoreKit.h>
+#define MR_SHORTHAND
+#import <MagicalRecord/MagicalRecord.h>
+#import <MagicalRecord/CoreData+MagicalRecord.h>
+
+#import <OnAirLog/Song.h>
+#import <OnAirLog/ShortenURL.h>
